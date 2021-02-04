2fced51bb613203824e99918585ba138f6a611b6
AvielCo <avielcohen15@gmail.com>
2021-02-04 14:12:14 +0200

    init

--- contracts/Vaccine.sol
+++ contracts/Vaccine.sol
@@ -1,5 +1,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Vaccine {
-    string Name;
+    uint public vaccinatedCount = 0;
+    struct Person{
+        string name;
+        string id;
+        uint256 vaccineDate;
+        string vaccineLocation;
+        bool firstVaccine;
+        bool secondVaccine;
+    }
 }
